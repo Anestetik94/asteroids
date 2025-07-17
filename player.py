87db195c93420901959e0545e80c4578dba26b3d
Anestetik94 <antoninkeclik@seznam.cz>
2025-07-17 16:40:53 +0200

    Refactor Player class: move draw method implementation and add new binary files

--- player.py
+++ player.py
@@ -6,6 +6,10 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0  # Player's rotation angle in degrees
     
+     def draw(self, screen):
+        points = self.triangle()
+        pygame.draw.polygon(screen, (255, 255, 255), points, 2)
+
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -13,7 +17,4 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
-    def draw(self, screen):
-        points = self.triangle()
-        pygame.draw.polygon(screen, (255, 255, 255), points, 2)
-        
+    
